adb910d8f4f4072623d3169cf071b5215092eb56
Tyler James <jamestg20@gmail.com>
2022-11-08 11:10:07 -0500

    Initial Commit

---
+++ TravelPlanningApp/TravelPlanningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelPlanningAppApp.swift
+//  TravelPlanningApp
+//
+//  Created by Tyler James on 11/8/22.
+//
+
+import SwiftUI
+
+@main
+struct TravelPlanningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
