520a402d22a01265fcf05541603bc7ea2398bc97
Tyler James <jamestg20@gmail.com>
2022-12-04 21:59:15 -0500

    merge

--- TravelPlanningApp/Views/DayViews/DayView.swift
+++ TravelPlanningApp/Views/DayViews/DayView.swift
@@ -20,24 +20,17 @@ struct DayView: View {
                         ContainerView(color: Color(hue: 1.0, saturation: 0.0, brightness: 0.896))
                         Text("\(activity.title)").foregroundColor(.black)
                     }
+                }
+            }
             NavigationLink{
                 CreateActivityView(day: day)
             } label: {
                 VStack{
-                    Image(systemName: "plus.circle")
-                        .resizable()
-                        .foregroundColor(Color(hue: 0.572, saturation: 0.792, brightness: 0.594))
-                        .aspectRatio(contentMode: .fill)
-                        .frame(width: 50, height: 50, alignment: .center)
-                        .padding(.top)
+                    AddButtonView(color: Color(hue: 0.572, saturation: 0.792, brightness: 0.594))
                     Text("Add Activity")
                         .foregroundColor((Color(hue: 0.572, saturation: 0.792, brightness: 0.594)))
                 }
             }
         }
-    }
-
-
-        
     }
 }
